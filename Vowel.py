1fdd7999dbf6912f8bfd10827063e2913e648314
AncyPD <52645950+AncyPD@users.noreply.github.com>
2019-07-17 20:27:15 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,8 +1,7 @@
 variable=input("")
-if len(variable)==1:
-  if variable=='a'or variable=='e'or variable=='i'or variable=='o'or variable=='u':
-    print("Vowel")
-  else:
-    print("Consonant")
+if variable=='a'or variable=='e'or variable=='i'or variable=='o'or variable=='u':
+  print("Vowel")
+elif variable=='b'or variable=='c'or variable=='d'or variable=='f'or variable=='g'or variable=='h'or variable=='j'or variable=='k'or variable=='l'or variable=='m'or variable=='n'or variable=='p'or variable=='q'or variable=='r'or variable=='s'or variable=='t'or variable=='v'or variable=='w'or variable=='x'or variable=='y'or variable=='z':
+  print("Consonant")
 else:
   print("Invalid")
