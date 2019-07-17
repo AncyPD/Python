4a13e3cfe22e102b97c44b02a4d9a9302ecb80c5
AncyPD <52645950+AncyPD@users.noreply.github.com>
2019-07-17 20:20:25 +0530

    Create Vowel.py

---
+++ Vowel.py
@@ -0,0 +1,8 @@
+variable=input("")
+if len(variable)==1:
+  if variable=='a'or variable=='e'or variable=='i'or variable=='o'or variable=='u':
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("Invalid")
