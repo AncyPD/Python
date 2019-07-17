49c9e5105db1ee0ff8522fff89d1a26ce6be344c
AncyPD <52645950+AncyPD@users.noreply.github.com>
2019-07-17 20:35:37 +0530

    Create Alphabet.py

---
+++ Alphabet.py
@@ -0,0 +1,5 @@
+variable=input("")
+if variable=='a'or variable=='e'or variable=='i'or variable=='o'or variable=='u'or variable=='b'or variable=='c'or variable=='d'or variable=='f'or variable=='g'or variable=='h'or variable=='j'or variable=='k'or variable=='l'or variable=='m'or variable=='n'or variable=='p'or variable=='q'or variable=='r'or variable=='s'or variable=='t'or variable=='v'or variable=='w'or variable=='x'or variable=='y'or variable=='z':
+  print("Alphabet")
+else:
+  print("No")
