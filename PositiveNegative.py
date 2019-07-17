7a8ed8e49d697d9340d6f7f1cf38f8ac29b80f10
AncyPD <52645950+AncyPD@users.noreply.github.com>
2019-07-17 16:54:30 +0530

    Update PositiveNegative.py

--- PositiveNegative.py
+++ PositiveNegative.py
@@ -1,9 +1,7 @@
-a=input("Enter a value")
+a=int(input(""))
 if a>0:
   print("Positive")
-elif a<0:
-  print("Negative")
 elif a==0:
   print("Zero")
 else:
-  print("Wrong Vale")
+  print("Negative")
